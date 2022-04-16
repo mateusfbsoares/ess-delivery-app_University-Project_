ac50adf4786298f7c132277cb2103f79bbe8347c
Zenio Oliveira <39632694+MisterZ99@users.noreply.github.com>
2022-04-16 16:34:14 -0300

    Generalização do atributo 'name' nos dois métodos

--- server/src/metodos_pagamento.ts
+++ server/src/metodos_pagamento.ts
@@ -10,15 +10,12 @@ export class Metodos_Pagamento {
 
   constructor(metodo_pagamento: Metodos_Pagamento) {
     this.type = metodo_pagamento.type;
+    this.name = metodo_pagamento.name;
     if (this.type == "Cartao de Credito" || this.type == "Cartao de Debito") {
       this.number = metodo_pagamento.number;
       this.name_titular = metodo_pagamento.name_titular;
       this.cvv = metodo_pagamento.cvv;
       this.flag = metodo_pagamento.flag;
-      this.name = metodo_pagamento.name;
-    }
-    if (this.type == "Pix") {
-      this.name = metodo_pagamento.name;
     }
     if (this.type == "PicPay"  || this.type == "PayPal") {
       this.email = metodo_pagamento.email;
@@ -26,14 +23,8 @@ export class Metodos_Pagamento {
   }
 
   update(metodo_pagamento: Metodos_Pagamento): void {
-      if (this.type == "Cartao de Credito" || this.type == "Cartao de Debito") {
-        this.name = metodo_pagamento.name;
-      }
-      if (this.type == "Pix") {
-        this.name = metodo_pagamento.name;
-      }
+      this.name = metodo_pagamento.name;
       if (this.type == "PicPay"  || this.type == "PayPal") {
-        this.name = metodo_pagamento.name;
         this.email = metodo_pagamento.email;
       }
   }
