4e3ad7106d30ee3b462443ec24950d59d24e83bf
Gabriel Oliveira <gthings2023@gmail.com>
2022-04-30 16:59:08 -0300

    menu de pagamentos e melhorando paginas de pagamento

--- web-app/src/app/insertcredit/insertcredit.component.ts
+++ web-app/src/app/insertcredit/insertcredit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Metodos_Pagamento } from '../classes/metodos_pagamento';
+import { user } from '../classes/users';
+import { InsertcreditService } from './insertcredit.service';
 
 @Component({
   selector: 'app-insertcredit',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InsertcreditComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,private aRoute: ActivatedRoute,private service:InsertcreditService) {
+   
+  
+  }
+
+  user:user;
+  ret:Metodos_Pagamento;
+  metodo:Metodos_Pagamento = new Metodos_Pagamento();
+
+  sendPost(): void {
+    // fazer confirmaçao dps
+    var confirmId = prompt("confirme seu Id");
+    var confirme = prompt("para confirmar escreva: CONFIRMAR  ")
+
+    this.service.create(this.user.id,this.metodo).then(res => {
+      this.ret = res;
+      console.log(this.ret);
+      if(this.ret != null){
+        console.log("deu bom 201");
+        this.goToComponentB()
+      }
+      else{
+
+          
+
+        console.log("deu ruim 400")
+      }
+    });
+
+  }
+
+  goToComponentB(): void {
+    this.router.navigate(['/user/pay'], {state: {user:this.user}});
+    console.log(this.user.name + " payment to addpay")
+  }
 
   ngOnInit() {
+    this.user = window.history.state.user;
+    this.metodo.type = "Cartao de Credito" ;
+   
   }
 
 }
