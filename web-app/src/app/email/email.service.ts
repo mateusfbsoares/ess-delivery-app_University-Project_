96298372dd151fa1020d66775fb0fdd5d1b8f7ac
mtfbs <mateusfbsoares@gmail.com>
2022-05-02 17:35:52 -0300

    fix: change alert message to user when re-sending an e-mail from orders list (succes vs failure)

--- web-app/src/app/email/email.service.ts
+++ web-app/src/app/email/email.service.ts
@@ -1,4 +1,4 @@
-import { Injectable }    from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { Order, User } from '../admin/user';
 import { LocalStorageService } from '../local-storage.service';
@@ -6,33 +6,35 @@ import { LocalStorageService } from '../local-storage.service';
 @Injectable()
 export class EmailService {
 
-  private headers = new Headers({'Content-Type': 'application/json'});
+  private headers = new Headers({ 'Content-Type': 'application/json' });
   private taURL = 'http://localhost:3000';
 
   constructor(private http: Http) { }
-  
+
   async sendEmailWithOrder(order: Order) {
     let localStorage = new LocalStorageService();
     const user: User = localStorage.get('user');
 
     const info = await this.sendEmail(user, order);
-    if(info)
-      alert('order sent ' + JSON.stringify(info));
+    if (info)
+      // alert('order sent ' + JSON.stringify(info));
+      alert("Email send successfuly")
     else
-      alert('problem sending');
+      // alert('problem sending');
+      alert('Email will be sent whithin 24 hours');
   }
-  
+
   sendEmail(user: User, order: Order) {
-    return this.http.post(this.taURL + `/payment/confirm/${user.id}`, JSON.stringify(order), {headers: this.headers})
+    return this.http.post(this.taURL + `/payment/confirm/${user.id}`, JSON.stringify(order), { headers: this.headers })
       .toPromise()
       .then(res => {
-        if (res.status === 201) {return res} else {return undefined;}
+        if (res.status === 201) { return res } else { return undefined; }
       })
       .catch(this.catch);
   }
 
-  private catch(erro: any): Promise<any>{
-    console.error('Oops, something went wrong',erro);
+  private catch(erro: any): Promise<any> {
+    console.error('Oops, something went wrong', erro);
     return Promise.reject(erro.message || erro);
   }
 }
